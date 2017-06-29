1162d8d7e1bc466fabaee5b7b5662bfdd6ac635f
heni123321 <henrik@haue.dk>
2017-06-29 09:29:46 +0200

    Javafx bug has been fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module jlinkgui {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.desktop;
-	//have to export this to all because FXMLLoader is invoking methods from the unnamed module
-	exports jlinkgui;
+	exports jlinkgui to javafx.graphics, javafx.fxml;
+	opens jlinkgui to javafx.fxml;
 }
